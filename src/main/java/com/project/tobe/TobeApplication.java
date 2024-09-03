3f3b0684e5527888e5ca581dbb95a2f712d428bd
shAn-kor <kjn0406@naver.com>
2024-09-03 09:45:35 +0900

    reset

---
+++ src/main/java/com/project/tobe/TobeApplication.java
@@ -0,0 +1,13 @@
+package com.project.tobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobeApplication.class, args);
+	}
+
+}
