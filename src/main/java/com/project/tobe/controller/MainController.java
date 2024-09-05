c102dba8ccfc80e1728c71f9895620648568d2e6
seonhwa <hellooohelloo@gmail.com>
2024-09-05 15:32:33 +0900

    0905

--- src/main/java/com/project/tobe/controller/MainController.java
+++ src/main/java/com/project/tobe/controller/MainController.java
@@ -10,7 +10,6 @@ public class MainController {
     @GetMapping("/{pageName}.do") //.do 해주세요
     public String page(@PathVariable String pageName) {
         System.out.println("뷰이름:" + pageName);
-
         return "layout"; //언제나 view화면으로 이동합니다.
     }
 
@@ -18,6 +17,5 @@ public class MainController {
     public String login(@PathVariable String pageName) {
         return "login";
     }
-
 
 }
