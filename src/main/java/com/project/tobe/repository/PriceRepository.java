5a72cc0dd7bfccdd9b2cf59bb68e5f9c060916e3
shAn-kor <kjn0406@naver.com>
2024-09-06 16:08:39 +0900

    layout sidebar 100% fill

--- src/main/java/com/project/tobe/repository/PriceRepository.java
+++ src/main/java/com/project/tobe/repository/PriceRepository.java
@@ -1,7 +1,12 @@
 package com.project.tobe.repository;
 
+import com.project.tobe.dto.PriceSearchDTO;
 import com.project.tobe.entity.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface PriceRepository extends JpaRepository<Price, Integer> {
+import java.util.List;
+
+public interface PriceRepository extends JpaRepository<Price, Long>, PriceCustomRepository, QuerydslPredicateExecutor<Price> {
+
 }
