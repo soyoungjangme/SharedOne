2fc8b919d8c1cdd0911c169f4cc97cf651cf413f
CUTLETER <hellooohelloo@gmail.com>
2024-09-08 08:37:40 +0900

    결재 모달창 추가 및 중복되는 상단바 css 제거

--- src/main/react/confirm/Confirm.js
+++ src/main/react/confirm/Confirm.js
@@ -4,7 +4,7 @@ import './Confirm.css';
 
 function Confirm() {
 
-    /*li 태그로 불러오기 연습*/
+    /*li 태그로 값 불러오기 연습 */
     let [confirmList, setConfirmList] = useState([]);
     const handleClick = async () => {
         let response = await fetch('/confirm/get?allId=1,2,3');
@@ -14,6 +14,16 @@ function Confirm() {
             setConfirmList(data);
         }
     };
+
+    /*모달 창 띄우기 연습*/
+    const [openModal, setOpenModal] = useState(false);
+    const handleOpenClick = () => {
+        setOpenModal(true);
+    };
+    const handleCloseClick = () => {
+        setOpenModal(false);
+    };
+
 
     return (
         <div>
@@ -42,15 +52,6 @@ function Confirm() {
                     </div>
 
                     <div className="filter-row">
-                        <label className="filter-label" htmlFor="confirmStatus">결재 여부</label>
-                        <select className="filter-select" id="confirmStatus" required>
-                            <option value="승인">승인</option>
-                            <option value="진행 중">대기</option>
-                            <option value="반려">반려</option>
-                        </select>
-                    </div>
-
-                    <div className="filter-row">
                         <label className="filter-label" htmlFor="confirmRegDate">등록 일자</label>
                         <input className="filter-input" type="date" id="confirmRegDate" required/>
                     </div>
@@ -60,8 +61,19 @@ function Confirm() {
                         <input className="filter-input" type="date" id="confirmConfirmDate" required/>
                     </div>
 
+                    <div className="filter-row">
+                        <label className="filter-label" htmlFor="confirmStatus">결재 여부</label>
+                        <select className="filter-select" id="confirmStatus" required>
+                            <option value="승인">승인</option>
+                            <option value="진행 중">대기</option>
+                            <option value="반려">반려</option>
+                        </select>
+                    </div>
+
                     <button className="filter-button" onClick={handleClick}>조회</button>
                 </div>
+
+                <button type="button" className="confirm-selected" onClick={handleOpenClick}>수정</button>
 
                 <table className="seacrh-table">
                     <thead>
@@ -145,6 +157,91 @@ function Confirm() {
                     </tbody>
                 </table>
             </div>
+
+{/*모달 창 띄우기 연습2*/}
+            {openModal && (
+                <div className="confirmRegist">
+                    <div className="fullBody">
+                        <div className="form-container">
+                            <div className="form-header">
+                                <h1>주문 및 결재 상세 조회</h1>
+
+                                <div className="btns">
+                                    <div className="btn-add">
+                                        <button>수정</button>
+                                    </div>
+                                    <div className="btn-close" onClick={handleCloseClick}>
+                                        <button> x </button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="RegistForm">
+                                <table className="formTable">
+                                    <tr>
+                                        <th colSpan="1"><label htmlFor="">고객사명</label></th>
+                                        <td colSpan="3"><input type="text" placeholder="필드 입력" /></td>
+                                        <th colSpan="1"><label htmlFor="">담당자명</label></th>
+                                        <td colSpan="3"><input type="text" placeholder="필드 입력" /></td>
+                                    </tr>
+
+                                    <tr>
+                                        <th><label htmlFor="">상품종류</label></th>
+                                        <td>
+                                            <select>
+                                                <option>도서</option>
+                                                <option>MD</option>
+                                                <option>기타</option>
+                                            </select>
+                                        </td>
+                                        <th><label htmlFor="">상품명</label></th>
+                                        <td><input type="text" placeholder="필드 입력"/></td>
+                                        <th><label htmlFor="">상품수량</label></th>
+                                        <td><input type="text" placeholder="필드 입력"/></td>
+                                    </tr>
+
+                                    <tr>
+                                        <th><label htmlFor="">총 금액</label></th>
+                                        <td><input type="text" placeholder="필드 입력"/></td>
+                                        <th><label htmlFor="">납품요청일</label></th>
+                                        <td><input type="date" placeholder="필드 입력"/></td>
+                                        <th><label htmlFor="">판매 시작날짜</label></th>
+                                        <td><input type="date" placeholder="필드 입력"/></td>
+                                        <th><label htmlFor="">판매 종료날짜</label></th>
+                                        <td><input type="date" placeholder="필드 입력"/></td>
+                                    </tr>
+
+                                    <tr>
+                                        <th><label htmlFor="">결재자</label></th>
+                                        <td><input type="text" placeholder="필드 입력" /></td>
+                                        <th><label htmlFor="">결재 여부</label></th>
+                                        <td>
+                                            <select>
+                                                <option value="pending">진행 중</option>
+                                                <option value="approved">승인</option>
+                                                <option value="rejected">반려</option>
+                                            </select>
+                                        </td>
+                                        <th><label htmlFor="">결재 승인일</label></th>
+                                        <td><input type="date" placeholder="필드 입력" /></td>
+                                    </tr>
+
+                                    <tr>
+                                        <th colSpan="1"><label htmlFor="">비고</label></th>
+                                        <td colSpan="7">
+                                            <input type="text" placeholder="필드 입력" />
+                                        </td>
+                                    </tr>
+                                </table>
+
+                                <div className="btn-add">
+                                    <button>추가</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
 
         </div>
     );
