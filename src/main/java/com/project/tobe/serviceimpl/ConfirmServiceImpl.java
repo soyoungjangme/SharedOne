2c4d4eb58966ebeb6d40b289a7353ed72c2fabd5
CUTLETER <hellooohelloo@gmail.com>
2024-09-11 03:24:50 +0900

    0911

--- src/main/java/com/project/tobe/serviceimpl/ConfirmServiceImpl.java
+++ src/main/java/com/project/tobe/serviceimpl/ConfirmServiceImpl.java
@@ -1,14 +1,31 @@
-package com.project.tobe.serviceimpl;
-
+package com.project.tobe.service.impl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.project.tobe.entity.Confirm;
 import com.project.tobe.repository.ConfirmRepository;
 import com.project.tobe.service.ConfirmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class ConfirmServiceImpl implements ConfirmService {
+    private final ConfirmRepository confirmRepository;
 
+    @Autowired
+    public ConfirmServiceImpl(ConfirmRepository confirmRepository) {
+        this.confirmRepository = confirmRepository;
+    }
+
+    @Override
+    public List<Confirm> saveConfirms(List<Confirm> confirms) {
+        return confirmRepository.saveAll(confirms);
+    }
+
+    @Override
+    public List<Confirm> getAllConfirms() {
+        return confirmRepository.findAll();
+    }
 }
