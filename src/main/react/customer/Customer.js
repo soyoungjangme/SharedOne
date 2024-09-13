a1a1de3d90ad6c6c6d96b19c86220e33180d1192
wjin01 <dnwls0787@gmail.com>
2024-09-13 21:42:58 +0900

    상품관리 기능 추가

--- src/main/react/customer/Customer.js
+++ src/main/react/customer/Customer.js
@@ -136,7 +136,7 @@ function Customer() {
 
     console.log("modifyItem은 ", modifyItem);
     console.log("setModifyItem ", setModifyItem);
-    
+
     console.log("값알아보기", customer[5]);
     console.log("값알아보기", customer[5]?.customerName);
 
@@ -151,7 +151,7 @@ function Customer() {
         console.log('받아온 주소:', fullAddress); // 콘솔에 주소 값 출력
         setContactNumber(fullAddress);
         closeAddressModal(); // 주소 입력 모달 닫기
-      };
+    };
 
 
 
@@ -160,7 +160,7 @@ function Customer() {
 
     return (
         <div className='fade_effect'>
-            
+
             <div className="pageHeader"><h1>  <i class="bi bi-people-fill"></i>  고객 리스트</h1></div>
             <div className="main-container">
 
@@ -333,13 +333,13 @@ function Customer() {
                             ))
                         ) : (
                             <tr>
-                            <td colSpan="14" style={{ textAlign: 'center', verticalAlign: 'middle' }}>등록된 상품이 없습니다
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-emoji-tear" viewBox="0 0 16 16" style={{ verticalAlign: 'middle' }}>
-                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                                    <path d="M6.831 11.43A3.1 3.1 0 0 1 8 11.196c.916 0 1.607.408 2.25.826.212.138.424-.069.282-.277-.564-.83-1.558-2.049-2.532-2.049-.53 0-1.066.361-1.536.824q.126.27.232.535.069.174.135.373ZM6 11.333C6 12.253 5.328 13 4.5 13S3 12.254 3 11.333c0-.706.882-2.29 1.294-2.99a.238.238 0 0 1 .412 0c.412.7 1.294 2.284 1.294 2.99M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5m-1.5-3A.5.5 0 0 1 10 3c1.162 0 2.35.584 2.947 1.776a.5.5 0 1 1-.894.448C11.649 4.416 10.838 4 10 4a.5.5 0 0 1-.5-.5M7 3.5a.5.5 0 0 0-.5-.5c-1.162 0-2.35.584-2.947 1.776a.5.5 0 1 0 .894.448C4.851 4.416 5.662 4 6.5 4a.5.5 0 0 0 .5-.5" />
-                                </svg>
-                            </td>
-                        </tr>
+                                <td colSpan="14" style={{ textAlign: 'center', verticalAlign: 'middle' }}>등록된 상품이 없습니다
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-emoji-tear" viewBox="0 0 16 16" style={{ verticalAlign: 'middle' }}>
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                        <path d="M6.831 11.43A3.1 3.1 0 0 1 8 11.196c.916 0 1.607.408 2.25.826.212.138.424-.069.282-.277-.564-.83-1.558-2.049-2.532-2.049-.53 0-1.066.361-1.536.824q.126.27.232.535.069.174.135.373ZM6 11.333C6 12.253 5.328 13 4.5 13S3 12.254 3 11.333c0-.706.882-2.29 1.294-2.99a.238.238 0 0 1 .412 0c.412.7 1.294 2.284 1.294 2.99M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5m-1.5-3A.5.5 0 0 1 10 3c1.162 0 2.35.584 2.947 1.776a.5.5 0 1 1-.894.448C11.649 4.416 10.838 4 10 4a.5.5 0 0 1-.5-.5M7 3.5a.5.5 0 0 0-.5-.5c-1.162 0-2.35.584-2.947 1.776a.5.5 0 1 0 .894.448C4.851 4.416 5.662 4 6.5 4a.5.5 0 0 0 .5-.5" />
+                                    </svg>
+                                </td>
+                            </tr>
                         )}
                         <tr>
                             <td colSpan="12"></td>
@@ -374,8 +374,8 @@ function Customer() {
                                         </div>
                                     </div>
                                     <button className="btn-open-modal" onClick={openAddressModal}>
-          주소 찾기
-        </button>
+                                        주소 찾기
+                                    </button>
                                 </div>
 
 
@@ -394,13 +394,13 @@ function Customer() {
 
 
                                         <tr>
-                                        <label htmlFor="contact-number">연락처:</label>
-          <input
-            id="contact-number"
-            type="text"
-            value={contactNumber}
-            placeholder="연락처 입력"
-          />
+                                            <label htmlFor="contact-number">연락처:</label>
+                                            <input
+                                                id="contact-number"
+                                                type="text"
+                                                value={contactNumber}
+                                                placeholder="연락처 입력"
+                                            />
 
 
                                             <th><label for="">연락처</label></th>
@@ -413,6 +413,7 @@ function Customer() {
                                             <td><input type="text" placeholder="필드 입력" /></td>
 
                                         </tr>
+
 
 
                                         <tr>
@@ -502,22 +503,22 @@ function Customer() {
                                 </div>
                             </div>
                         </div>
-                         {/* 주소 모달 */}
-      {isAddressModalVisible && (
-        <div className="confirmRegist">
-          <div className="fullBody">
-            <div className="form-container">
-              <button className="close-btn" onClick={closeAddressModal}>
-                &times;
-              </button>
-              <div className="form-header">
-                <h1>주소 입력</h1>
-              </div>
-              <AddressInput onAddressConfirm={handleAddressConfirm} /> {/* 주소 입력 컴포넌트 */}
-                        </div>
-                    </div>
-                </div>
-            )}
+                        {/* 주소 모달 */}
+                        {isAddressModalVisible && (
+                            <div className="confirmRegist">
+                                <div className="fullBody">
+                                    <div className="form-container">
+                                        <button className="close-btn" onClick={closeAddressModal}>
+                                            &times;
+                                        </button>
+                                        <div className="form-header">
+                                            <h1>주소 입력</h1>
+                                        </div>
+                                        <AddressInput onAddressConfirm={handleAddressConfirm} /> {/* 주소 입력 컴포넌트 */}
+                                    </div>
+                                </div>
+                            </div>
+                        )}
                     </div>
 
 
