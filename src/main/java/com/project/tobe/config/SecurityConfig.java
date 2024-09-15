c51d5c89f3a591f3d029df2f4b2408bc0e066f4f
shAn-kor <kjn0406@naver.com>
2024-09-15 15:24:15 +0900

    shAn fix security

--- src/main/java/com/project/tobe/config/SecurityConfig.java
+++ src/main/java/com/project/tobe/config/SecurityConfig.java
@@ -35,9 +35,9 @@ public class SecurityConfig {
 
     http
         .authorizeRequests()
-
-        .antMatchers("/main.do", "/myPage.do", "/product.do", "/customer.do", "/order.do", "/price.do").authenticated() // ".do"로 끝나는 모든 요청은 인증된 사용자만 접근 가능
+        .antMatchers("/login.user").permitAll() // 로그인 페이지는 인증 없이 접근 허용
         .antMatchers("/employee.do").hasAnyRole(UserRole.S.toString(), UserRole.A.toString()) // "/employee.do" 경로는 "A","S" 역할을 가진 사용자만 접근 가능
+        .antMatchers("/*.do").authenticated()
         .anyRequest().permitAll()
         .and()
         .formLogin()
@@ -55,6 +55,28 @@ public class SecurityConfig {
         .and()
 			  .exceptionHandling()
         .accessDeniedPage("/accessDenied"); //accessDenied
+
+//    http
+//            .authorizeRequests()
+//            .antMatchers("/main.do", "/myPage.do", "/product.do", "/customer.do", "/order.do", "/price.do").authenticated() // ".do"로 끝나는 모든 요청은 인증된 사용자만 접근 가능
+//            .antMatchers("/employee.do").hasAnyRole(UserRole.S.toString(), UserRole.A.toString()) // "/employee.do" 경로는 "A","S" 역할을 가진 사용자만 접근 가능
+//            .anyRequest().permitAll()
+//            .and()
+//            .formLogin()
+//            .loginPage("/login.user")
+//            .usernameParameter("employeeId")
+//            .passwordParameter("employeePw")
+//            .loginProcessingUrl("/loginForm") //로그인 페이지를 가로채 시큐리티가 제공하는 클래스로 로그인을 연결합니다.
+//            .defaultSuccessUrl("/main.do")
+//            .failureUrl("/login.user?err=true")
+//            .and()
+//            .logout()//로그인 성공시 이동될 URL을 적습니다
+//            .logoutUrl("/logout")
+//            .logoutSuccessUrl("/login.user")
+//            .invalidateHttpSession(true).deleteCookies("JSESSIONID")
+//            .and()
+//            .exceptionHandling()
+//            .accessDeniedPage("/accessDenied"); //accessDenied
 
     return http.build();
   }
