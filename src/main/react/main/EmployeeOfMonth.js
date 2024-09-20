2a47c7a201bffe89ee06a7853084e9666dad8882
shAn-kor <kjn0406@naver.com>
2024-09-20 16:21:27 +0900

    대쉬보드 사진 삭제

--- src/main/react/main/EmployeeOfMonth.js
+++ src/main/react/main/EmployeeOfMonth.js
@@ -4,11 +4,6 @@ import React from 'react';
 const EmployeeOfMonth = () => {
     return (
         <div className="Employee-ranker">
-            <img
-                src="https://file2.nocutnews.co.kr/newsroom/image/2023/06/06/202306060141536125_0.jpg"
-                alt="Employee of the Month"
-                className="Employee-photo"
-            />
             <div id="Employee-of-month" style={{ textAlign: 'center', marginTop: '30px' }}>
                 Thomas Mueller (매출액: 1,500만원)
             </div>
